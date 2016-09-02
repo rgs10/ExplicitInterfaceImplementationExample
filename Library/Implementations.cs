15ee96b66a6f823ab5dfbf5c53b36e86d6dd9ae8
SmithR <SmithR@LAPTOP-RSMITH>
2016-09-02 15:30:11 +0100

    Add project files.

---
+++ Library/Implementations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Library
+{
+    public class StandardCatalog : ISaveable, IPersistable
+    {
+        public void Load()
+        {
+        }
+
+        public string Save()
+        {
+            return "Catalog Save";
+        }
+    }
+
+    public class ExplicitCatalog : ISaveable, IPersistable
+    {
+        //public string Save()
+        //{
+        //    return "Catalog Save";
+        //}
+        string ISaveable.Save()
+        {
+            return "ISaveable Save";
+        }
+        string IPersistable.Save()
+        {
+            return "IPersistable Save";
+        }
+    }
+
+}
